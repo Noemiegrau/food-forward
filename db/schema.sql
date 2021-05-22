7b6e08b5f67ccd19e97b54f47a9dc1b30e954814
Wayne Edwards <waedwards42@yahoo.com>
2021-05-22 16:42:04 -0700

    created seeds.sql and updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
+Create TABLE staff(
+    iD INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    staff_number VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL
+);
+
 CREATE TABLE customers (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -5,4 +14,27 @@ CREATE TABLE customers (
     age INTEGER NOT NULL,
     in_household INTEGER NOT NULL,
     package INTEGER NOT NULL,
-    date_received VARCHAR(30) NOT NULL
+    date_received DATE NOT NULL
+);
+
+CREATE TABLE post_likes (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    staff_id INTEGER NOT NULL;
+    post_id INTEGER NOT NULL
+);
+
+CREATE TABLE post (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    post_text VARCHAR(60) CHARACTER SET utf8,
+    staff_id INTEGER
+);
+
+CREATE TABLE comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(60) CHARACTER SET utf8,
+    staff_id INTEGER,
+    post_id INTEGER
+);
+
+
+
