bd58ec1f130e6097a4860c08d4edac6e9367dcd6
Wayne Edwards <waedwards42@yahoo.com>
2021-05-22 10:50:01 -0700

    created tables and db.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
-DROP DATABASE IF EXISTS food_bank;
-
-CREATE DATABASE food_bank;
+CREATE TABLE customers (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    age INTEGER NOT NULL,
+    in_household INTEGER NOT NULL,
+    package INTEGER NOT NULL,
+    date_received VARCHAR(30) NOT NULL
