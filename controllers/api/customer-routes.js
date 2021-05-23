96a8b94623896afe66cadf97c7a326a1dda9473a
Noemiegrau <noemiegrau@outlook.fr>
2021-05-22 17:58:51 -0700

    added customer routes

--- controllers/api/customer-routes.js
+++ controllers/api/customer-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Customer = require('../../models/Customer');
 const { Staff, Like, Comment, Post } = require('../../models');
+const { aggregate } = require('../../models/Customer');
 
 // get all customers
 router.get('/', (req, res) => {
@@ -24,6 +25,73 @@ router.get('/:id', (req, res) => {
             'package',
             'date_received'
         ],
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCustomerData => {
+        if (!dbCustomerData) {
+          res.status(404).json({ message: 'No customer found with this id' });
+          return;
+        }
+        res.json(dbCustomerData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// create a customer
+router.post('/', (req, res) => {
+    Customer.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        age: req.body.age,
+        household: req.body.household,
+        package: req.body.package,
+        date_received: req.body.date_received
+    })
+    .then(dbCustomerData => res.json(dbCustomerData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// update a customer
+router.put('/:id', (req, res) => {
+    Customer.update(
+    {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        age: req.body.age,
+        household: req.body.household,
+        package: req.body.package,
+        date_received: req.body.date_received
+    },
+    {
+        where: {
+        id: req.params.id
+        }
+    }
+    )
+    .then(dbCustomerData => {
+        if (!dbCustomerData) {
+          res.status(404).json({ message: 'No customer found with this id' });
+          return;
+        }
+        res.json(dbCustomerData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// delete a customer
+router.delete('/:id', (req, res) => {
+    Customer.destroy({
         where: {
             id: req.params.id
         }
