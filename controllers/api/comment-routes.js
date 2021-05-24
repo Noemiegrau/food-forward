f86b68627b5fae2bc5cb4d5336506db88248ccb4
Noemiegrau <noemiegrau@outlook.fr>
2021-05-24 15:59:41 -0700

    added staff routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,31 @@
 const router = require('express').Router();
-const { Customer, Staff, Like, Comment, Post } = require('../../models');
+const { Staff, Like, Comment, Post } = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll()
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+router.post('/', (req, res) => {
+    // check the session
+      // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+    if (req.session) {
+      Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        // use the id from the session
+        user_id: req.session.user_id
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    }
+  });
 
 module.exports = router;
