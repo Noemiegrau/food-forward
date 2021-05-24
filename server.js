c546827f55aa8ef1c9cb9d05b72aabb9f11b0b75
Noemiegrau <noemiegrau@outlook.fr>
2021-05-24 11:40:55 -0700

    added nodemailer package and code

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const express = require('express');
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
+require("dotenv").config();
+const cors = require("cors");
 const routes = require('./controllers');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
@@ -30,8 +34,61 @@ app.set('view engine', 'handlebars');
 // Express MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// cors
+app.use(cors({ origin: "*" }));
+
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.static('views/images')); 
+
+// create transporter object
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com", //email provider is gmail
+  port: 587, //if not working try 25, 465 or 2525
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+// verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+// POST route for NODEMAILER contact form
+app.post("/send", (req, res) => {
+// Accepts the form data submitted and parse it using multiparty
+let form = new multiparty.Form();
+let data = {};
+form.parse(req, function (err, fields) {
+  console.log(fields);
+  Object.keys(fields).forEach(function (property) {
+  data[property] = fields[property].toString();
+});
+
+// Create a mail object
+const mail = {
+  from: data.name,
+  to: process.env.EMAIL,
+  subject: data.subject,
+  text: `${data.name} <${data.email}> \n${data.message}`,
+};
+
+// Use transporter.sendMail() to send the email
+transporter.sendMail(mail, (err, data) => {
+  if (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong.");
+  } else {
+    res.status(200).send("Email successfully sent to recipient!")
+  }
+});
+});
+});
 
 // turn on routes
 app.use(routes);
@@ -42,7 +99,7 @@ sequelize.sync({ force: false}).then(() => {
 });
 
 
-
+// cf nodemailer tutorial at https://github.com/victoria-lo/Nodemailer-Tutorial
 
 
 
