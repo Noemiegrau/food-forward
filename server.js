7967f80f3a54228f387e5d91c58cb5dee033e1d2
Noemiegrau <noemiegrau@outlook.fr>
2021-05-24 22:57:46 -0700

    test email form

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.use(express.static('views/images'));
 
 // create transporter object
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", //email provider is gmail
-  port: 465, //if not working try 25, 465 or 2525
+  host: "smtp-mail.outlook.com", //email provider is gmail
+  port: 587, //if not working try 25, 465, 587 or 2525
   auth: {
     user: process.env.DB_EMAIL,
     pass: process.env.DB_PASS,
