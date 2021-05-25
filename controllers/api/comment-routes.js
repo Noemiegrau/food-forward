53585c1b734bb36043ed7e7d3fe0dadd024f000a
Noemiegrau <noemiegrau@outlook.fr>
2021-05-24 19:59:55 -0700

    update comment route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -29,4 +29,23 @@ router.post('/', (req, res) => {
     }
   });
 
+  router.delete('/:id', (req, res) => {
+    Comment.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbCommentData => {
+        if (!dbCommentData) {
+          res.status(404).json({ message: 'No comment found with this id!' });
+          return;
+        }
+        res.json(dbCommentData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 module.exports = router;
